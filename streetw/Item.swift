8553b29011b0d5b50ee137f9d4aea4b1a9837e9e
D. Kern <hottorun@pm.me>
2026-08-09 16:37:56 +0300

    Initial Commit

---
+++ streetw/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  streetw
+//
+//  Created by Dimitris Kern on 09.08.26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
